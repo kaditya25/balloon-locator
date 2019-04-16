3ec03d726dfe3880d64155ab3093905e2aaee245
Todd Humphreys <todd.humphreys@mail.utexas.edu>
2019-04-16 14:23:41 -0500

    wip

--- opencv-lecture/main_pre-lecture.cc
+++ opencv-lecture/main_pre-lecture.cc
@@ -8,8 +8,10 @@ int main(int argc, char** argv) {
 
   using namespace cv;
 
+  // Open video file via a VideoCapture object named camera
   std::string filename = "./input.m4v";
   VideoCapture camera(filename);
+  // Check to ensure the video file opened properly
   if(!camera.isOpened()) {
     std::cout << "Failed to open " << filename << std::endl; 
     return EXIT_FAILURE;
